cf48fda57d5c41cea93891b190d856713b1ea2b0
Paul Thiberville <paul.thiberville@live.fr>
2022-07-06 19:11:59 +0200

    meta og

--- components/layout.js
+++ components/layout.js
@@ -15,6 +15,15 @@ export default function Layout({ children }) {
         <meta property="og:image" content="https://i.ibb.co/YdjkkDx/favz.png" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="next-favz.vercel.app" />
+        <meta property="twitter:url" content="https://next-favz.vercel.app/" />
+        <meta name="twitter:title" content="Favz" />
+        <meta name="twitter:description" content="Save your bookmarks" />
+        <meta
+          name="twitter:image"
+          content="https://i.ibb.co/YdjkkDx/favz.png"
+        />{" "}
       </Head>
       <header className={styles.header}>
         <Image height={50} width={50} src="/favz.png" alt={"Favz"}></Image>
